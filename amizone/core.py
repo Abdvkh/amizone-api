450231278e2069a56ec333052df366647a2790fa
abdvkh <e-address@bk.ru>
2021-03-10 22:48:36 +0500

    Introduce workflow with local JSON data of Amizone using JSONDatabase class to have faster response rather than requesting each time;

--- amizone/core.py
+++ amizone/core.py
@@ -1,4 +1,7 @@
 import os
+import json
+import time
+
 import requests
 import bs4
 
@@ -8,6 +11,29 @@ load_dotenv()
 
 URL = os.getenv('URL', 'https://s.amizone.net/')
 URL_LOGIN = os.getenv('URL_LOGIN', 'https://s.amizone.net/')
+
+
+class JSONDatabase:
+    DATABASE_PATH = os.getenv('JSON_DATABASE_PATH', 'database/')
+
+    @classmethod
+    def store_user(self, username, information):
+
+        if not os.path.exists(self.DATABASE_PATH):
+            os.makedirs(self.DATABASE_PATH)
+
+        file = open(self.DATABASE_PATH + username + '.json', 'w+')
+
+        json.dump(information, file)
+
+    @classmethod
+    def information_exists(self, username):
+        return os.path.exists(self.DATABASE_PATH + username + '.json')
+
+    @classmethod
+    def get(self, username, key):
+        file = open(self.DATABASE_PATH + username + '.json', 'r')
+        return json.load(file).get(key)
 
 
 class Cookies:
@@ -31,6 +57,7 @@ class Cookies:
 
 
 class Amizone:
+    JSONDatabase = 'JSON'
 
     def __init__(self, username, password):
         self.username = username
@@ -43,53 +70,64 @@ class Amizone:
         self.login(username, password)
 
     def login(self, username, password):
-        self.c.login(username, password)
-        self.r.cookies = self.c.cookies
+        if not JSONDatabase.information_exists(self.username):
+            self.c.login(username, password)
+            self.r.cookies = self.c.cookies
 
     def get_profile(self):
-        a = self.r.get(URL + "Electives/NewCourseCoding?X-Requested-With=XMLHttpRequest")
-        b = bs4.BeautifulSoup(a.content, 'html.parser')
+        result = {}
 
-        row1 = [x.text for x in b.find_all("div", attrs={"class": "col-md-3"})]
-        row2 = [x.text for x in b.find_all("div", attrs={"class": "col-md-2"})]
+        if JSONDatabase.information_exists(self.username):
+            result = JSONDatabase.get(self.username, 'profile')
+        else:
+            a = self.r.get(URL + "Electives/NewCourseCoding?X-Requested-With=XMLHttpRequest")
+            b = bs4.BeautifulSoup(a.content, 'html.parser')
 
-        name = row1[0]
-        enrollment = row1[1]
-        programme = row2[0]
-        sem = row2[1]
-        pass_year = row2[2]
-        img = "https://amizone.net/amizone/Images/Signatures/" + self.username + "_P.png"
+            row1 = [x.text for x in b.find_all("div", attrs={"class": "col-md-3"})]
+            row2 = [x.text for x in b.find_all("div", attrs={"class": "col-md-2"})]
 
-        return {
-            'name': name,
-            'enrollment': enrollment,
-            'programme': programme,
-            'sem': sem,
-            'pass_year': pass_year,
-            'img_url': img,
-        }
+            name = row1[0]
+            enrollment = row1[1]
+            programme = row2[0]
+            sem = row2[1]
+            pass_year = row2[2]
+            img = "https://amizone.net/amizone/Images/Signatures/" + self.username + "_P.png"
+
+            result = {
+                'name': name,
+                'enrollment': enrollment,
+                'programme': programme,
+                'sem': sem,
+                'pass_year': pass_year,
+                'img_url': img,
+            }
+
+        return result
 
     def get_courses(self):
         result = []
 
-        a = self.r.get(URL + "Academics/MyCourses?X-Requested-With=XMLHttpRequest")
-        b = bs4.BeautifulSoup(a.content, 'html.parser')
+        if JSONDatabase.information_exists(self.username):
+            result = JSONDatabase.get(self.username, 'courses')
+        else:
+            a = self.r.get(URL + "Academics/MyCourses?X-Requested-With=XMLHttpRequest")
+            b = bs4.BeautifulSoup(a.content, 'html.parser')
 
-        course_code = [c.text.strip() for c in b.find_all(attrs={'data-title': "Course Code"})]
-        course_name = [c.text.strip() for c in b.find_all(attrs={'data-title': "Course Name"})]
-        attendance = [c.text.strip() for c in b.find_all(attrs={'data-title': "Attendance"})]
-        syllabus = [c.decode_contents() for c in b.find_all(attrs={'data-title': "Course Syllabus"})]
+            course_code = [c.text.strip() for c in b.find_all(attrs={'data-title': "Course Code"})]
+            course_name = [c.text.strip() for c in b.find_all(attrs={'data-title': "Course Name"})]
+            attendance = [c.text.strip() for c in b.find_all(attrs={'data-title': "Attendance"})]
+            syllabus = [c.decode_contents() for c in b.find_all(attrs={'data-title': "Course Syllabus"})]
 
-        # this returned a list(string) of anchor tags so the below code is to extract href from it
-        syllabus = [i[i.find('"') + 1:i.find('"', i.find('"') + 1)] for i in syllabus]
+            # this returned a list(string) of anchor tags so the below code is to extract href from it
+            syllabus = [i[i.find('"') + 1:i.find('"', i.find('"') + 1)] for i in syllabus]
 
-        for i in range(len(course_code)):
-            result.append({
-                'course_code': course_code[i],
-                'course_name': course_name[i],
-                'attendance': attendance[i],
-                'syllabus_download_url': syllabus[i],
-            })
+            for i in range(len(course_code)):
+                result.append({
+                    'course_code': course_code[i],
+                    'course_name': course_name[i],
+                    'attendance': attendance[i],
+                    'syllabus_download_url': syllabus[i],
+                })
 
         return result
 
@@ -99,94 +137,126 @@ class Amizone:
             'combined_results': []
         }
 
-        a = self.r.get(URL + "Examination/Examination?X-Requested-With=XMLHttpRequest")
-        b = bs4.BeautifulSoup(a.content, 'html.parser')
 
-        course_code = [c.text.strip() for c in b.find_all(attrs={'data-title': "Course Code"})]
-        course_title = [c.text.strip() for c in b.find_all(attrs={'data-title': "Course Title"})]
-        grade_obtained = [c.text.strip() for c in b.find_all(attrs={'data-title': "Go"})]
-        grade_point = [c.text.strip() for c in b.find_all(attrs={'data-title': "GP"})]
+        if JSONDatabase.information_exists(self.username):
+            result = JSONDatabase.get(self.username, 'results')
+        else:
+            a = self.r.get(URL + "Examination/Examination?X-Requested-With=XMLHttpRequest")
+            b = bs4.BeautifulSoup(a.content, 'html.parser')
 
-        sgpa = [x.text.strip() for x in b.find_all(attrs={'data-title': "SGPA"})]
-        cgpa = [x.text.strip() for x in b.find_all(attrs={'data-title': "CGPA"})]
+            course_code = [c.text.strip() for c in b.find_all(attrs={'data-title': "Course Code"})]
+            course_title = [c.text.strip() for c in b.find_all(attrs={'data-title': "Course Title"})]
+            grade_obtained = [c.text.strip() for c in b.find_all(attrs={'data-title': "Go"})]
+            grade_point = [c.text.strip() for c in b.find_all(attrs={'data-title': "GP"})]
 
-        results['semester_number'] = str(len(sgpa))
+            sgpa = [x.text.strip() for x in b.find_all(attrs={'data-title': "SGPA"})]
+            cgpa = [x.text.strip() for x in b.find_all(attrs={'data-title': "CGPA"})]
 
-        for i in range(len(course_code)):
-            results['subjects'].append({
-                'course_code': course_code[i],
-                'course_title': course_title[i],
-                'grade_obtained': grade_obtained[i],
-                'grade_point': grade_point[i],
-            })
+            results['semester_number'] = str(len(sgpa))
 
-        for i in range(len(sgpa)):
-            results['combined_results'].append({
-                'sgpa': sgpa[i],
-                'cgpa': cgpa[i]
-            })
+            for i in range(len(course_code)):
+                results['subjects'].append({
+                    'course_code': course_code[i],
+                    'course_title': course_title[i],
+                    'grade_obtained': grade_obtained[i],
+                    'grade_point': grade_point[i],
+                })
+
+            for i in range(len(sgpa)):
+                results['combined_results'].append({
+                    'sgpa': sgpa[i],
+                    'cgpa': cgpa[i]
+                })
 
         return results
 
     def get_faculties(self):
         result = []
 
-        a = self.r.get(URL + "FacultyFeeback/FacultyFeedback?X-Requested-With=XMLHttpRequest")
-        b = bs4.BeautifulSoup(a.content, 'html.parser')
+        if JSONDatabase.information_exists(self.username):
+            result = JSONDatabase.get(self.username, 'faculties')
+        else:
+            a = self.r.get(URL + "FacultyFeeback/FacultyFeedback?X-Requested-With=XMLHttpRequest")
+            b = bs4.BeautifulSoup(a.content, 'html.parser')
 
-        faculties = [x.text.strip() for x in b.find_all(attrs={"class": "faculty-name"})]
-        subjects = [x.text.strip() for x in b.find_all(attrs={"class": "subject"})]
-        images = [x["src"] for x in b.find_all(attrs={"class": "img-responsive"})]
+            faculties = [x.text.strip() for x in b.find_all(attrs={"class": "faculty-name"})]
+            subjects = [x.text.strip() for x in b.find_all(attrs={"class": "subject"})]
+            images = [x["src"] for x in b.find_all(attrs={"class": "img-responsive"})]
 
-        for i in range(len(subjects)):
-            result.append({
-                'subject': subjects[i],
-                'faculty': faculties[i],
-                'image_url': images[i],
-            })
+            for i in range(len(subjects)):
+                result.append({
+                    'subject': subjects[i],
+                    'faculty': faculties[i],
+                    'image_url': images[i],
+                })
 
         return result
 
     def get_exam_schedule(self):
         result = []
 
-        a = self.r.get(URL + "Examination/ExamSchedule?X-Requested-With=XMLHttpRequest")
-        b = bs4.BeautifulSoup(a.content, 'html.parser')
+        if JSONDatabase.information_exists(self.username):
+            result = JSONDatabase.get(self.username, 'exam_schedule')
+        else:
+            a = self.r.get(URL + "Examination/ExamSchedule?X-Requested-With=XMLHttpRequest")
+            b = bs4.BeautifulSoup(a.content, 'html.parser')
 
-        course_code = [c.text.strip() for c in b.find_all(attrs={'data-title': "Course Code"})]
-        course_title = [c.text.strip() for c in b.find_all(attrs={'data-title': "Course Title"})]
-        exam_date = [c.text.strip() for c in b.find_all(attrs={'data-title': "Exam Date"})]
-        exam_time = [c.text.strip() for c in b.find_all(attrs={'data-title': "Time"})]
+            course_code = [c.text.strip() for c in b.find_all(attrs={'data-title': "Course Code"})]
+            course_title = [c.text.strip() for c in b.find_all(attrs={'data-title': "Course Title"})]
+            exam_date = [c.text.strip() for c in b.find_all(attrs={'data-title': "Exam Date"})]
+            exam_time = [c.text.strip() for c in b.find_all(attrs={'data-title': "Time"})]
 
-        for i in range(len(course_code)):
-            result.append({
-                'course_code': course_code[i],
-                'course_title': course_title[i],
-                'exam_date': exam_date[i],
-                'exam_time': exam_time[i],
-            })
+            for i in range(len(course_code)):
+                result.append({
+                    'course_code': course_code[i],
+                    'course_title': course_title[i],
+                    'exam_date': exam_date[i],
+                    'exam_time': exam_time[i],
+                })
 
         return result
 
     def get_timetable(self):
         result = []
 
-        a = self.r.get(URL + "TimeTable/Home?X-Requested-With=XMLHttpRequest")
-        b = bs4.BeautifulSoup(a.content, 'html.parser')
+        if JSONDatabase.information_exists(self.username):
+            result = JSONDatabase.get(self.username, 'timetable')
+        else:
+            a = self.r.get(URL + "TimeTable/Home?X-Requested-With=XMLHttpRequest")
+            b = bs4.BeautifulSoup(a.content, 'html.parser')
 
-        course_code = [x.text.strip() for x in b.find_all(attrs={"class": "course-code"})]
-        course_teacher = [c.text.strip() for c in b.find_all(attrs={'class': "course-teacher"})]
-        class_location = [x.text.strip() for x in b.find_all(attrs={"class": "class-loc"})]
-        exam_time = [x.text.strip() for x in b.find_all(attrs={"class": "class-time"})]
+            course_code = [x.text.strip() for x in b.find_all(attrs={"class": "course-code"})]
+            course_teacher = [c.text.strip() for c in b.find_all(attrs={'class': "course-teacher"})]
+            class_location = [x.text.strip() for x in b.find_all(attrs={"class": "class-loc"})]
+            exam_time = [x.text.strip() for x in b.find_all(attrs={"class": "class-time"})]
 
-        for i in range(len(course_code)):
-            result.append({
-                'course_code': course_code[i],
-                'course_teacher': course_teacher[i],
-                'class_location': class_location[i],
-                'exam_time': exam_time[i],
-            })
+            for i in range(len(course_code)):
+                result.append({
+                    'course_code': course_code[i],
+                    'course_teacher': course_teacher[i],
+                    'class_location': class_location[i],
+                    'exam_time': exam_time[i],
+                })
+
         return result
+
+    def get_all_information(self):
+        return {
+            'last_updated_at': time.time(),
+            'profile': self.get_profile(),
+            'courses': self.get_courses(),
+            'timetable': self.get_timetable(),
+            'faculties': self.get_faculties(),
+            'exam_results': self.get_exam_results(),
+            'exam_schedule': self.get_exam_schedule()
+        }
+
+    def save_all_information(self, db_type='JSON'):
+        if db_type == self.JSONDatabase:
+            JSONDatabase.store_user(
+                self.username,
+                self.get_all_information()
+            )
 
 
 if __name__ == "__main__":
@@ -197,11 +267,15 @@ if __name__ == "__main__":
 
     instance.login(username, password)
 
-    print(instance.get_profile())
+    instance.save_all_information()
 
-    print(instance.get_courses())
+    print(instance.get_all_information())
 
-    print(instance.get_faculties())
-    print(instance.get_exam_results())
-    print(instance.get_exam_schedule())
-    print(instance.get_timetable())
+    # print(instance.get_profile())
+    #
+    # print(instance.get_courses())
+    #
+    # print(instance.get_faculties())
+    # print(instance.get_exam_results())
+    # print(instance.get_exam_schedule())
+    # print(instance.get_timetable())
