c054a372214f22453fa6778bcaaf782053027829
abdvkh <e-address@bk.ru>
2021-03-14 14:42:32 +0500

    Separate database interfase; Some refactoring;

---
+++ database.py
@@ -0,0 +1,25 @@
+import os
+import json
+
+DATABASE_PATH = os.getenv('JSON_DATABASE_PATH', 'database/')
+
+
+class JSONDatabase:
+
+    @staticmethod
+    def store_user(username, information):
+        if not os.path.exists(DATABASE_PATH):
+            os.makedirs(DATABASE_PATH)
+
+        file = open(DATABASE_PATH + username + '.json', 'w+')
+
+        json.dump(information, file)
+
+    @staticmethod
+    def information_exists(username):
+        return os.path.exists(DATABASE_PATH + username + '.json')
+
+    @staticmethod
+    def get(username, key):
+        file = open(DATABASE_PATH + username + '.json', 'r')
+        return json.load(file).get(key)
